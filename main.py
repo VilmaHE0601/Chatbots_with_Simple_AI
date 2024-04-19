216ab86fc15ea526925f1db1e5024a9f0bac4ba3
VilmaHE0601 <146425185+VilmaHE0601@users.noreply.github.com>
2024-04-19 12:14:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,83 @@ import os
 ## 1. Create Node Class
 class Node:
 
-  def __init__(self, value, answer="", children=[]):
+  def __init__(self, value, answer="", children=[], good_result=False):
     self.value = value
     self.answer = answer
     self.children = children
+    self.good_result = good_result
     #setup the Node
-    # every node has a value, answer, and a childre, but in default they are defined to be empty
+    
 
+### 2. Create a Decision Tree
+###Happy Relaxed Today
+MojitoNode2 = Node(" ", answer="No")
+MojitoNode1 = Node("Master has given Dobby a good choice!",
+                   good_result=True,
+                   answer="Yes")
 
-### 2. Create an Example Decision Tree
-node1 = Node('Do Project', answer='Yes')
-node2 = Node('Sleep', answer='No')
-node3 = Node('Dobby is free! 🎉')
-node4 = Node('"Oh! Dobby must not say!"', answer='No', children=[node1, node2])
-node5 = Node('a')
-node6 = Node('b')
-node7 = Node('c')
-node8 = Node('Dobby', answer='Maybe', children=[node5, node6, node7])
-root = Node(
-  'Dobby feels that the sunshine is especially warm today. Master, do you have any plans for today?', children=[node1, node4, node8])
+TequilaSunriseNode2 = Node(" ", answer="No")
+TequilaSunriseNode1 = Node("Weee!!", good_result=True, answer="Yes")
+
+French75Node2 = Node(" ", answer="No")
+French75Node1 = Node("Dobby is happy!", good_result=True, answer="Yes")
+
+MojitoNode = Node("Mojito", answer="Rum", children=[MojitoNode1, MojitoNode2])
+TequilaSunriseNode = Node("Tequila Sunrise",
+                          answer="Tequila",
+                          children=[TequilaSunriseNode1, TequilaSunriseNode2])
+French75Node = Node("French 75",
+                    answer="Gin",
+                    children=[French75Node1, French75Node2])
+
+option2b = Node("Dobby is freeeee!!!", good_result=True, answer="No")
+option2a = Node("What base wine does master prefer?",
+                answer="Yes",
+                children=[French75Node, TequilaSunriseNode, MojitoNode])
+
+###Feeling Down Today
+AbsintheDripNode2 = Node(" ", answer="No")
+AbsintheDripNode1 = Node(
+  "Dobby doesn't like Absinthe Drip, but the master's preference is most important!",
+  good_result=True,
+  answer="Yes")
+
+OldFashionedNode2 = Node(" ", answer="No")
+OldFashionedNode1 = Node("Dobby loves Old Fashioned!",
+                         good_result=True,
+                         answer="Yes")
+
+BlackRussianNode2 = Node(" ", answer="No")
+BlackRussianNode1 = Node("Dobby likes Black Russian too!",
+                         good_result=True,
+                         answer='Yes')
+
+AbsintheNode = Node("Absinthe Drip",
+                    answer="Absinthe",
+                    children=[AbsintheDripNode1, AbsintheDripNode2])
+WhiskeyNode = Node("Old Fashioned",
+                   answer="Whiskey",
+                   children=[OldFashionedNode1, OldFashionedNode2])
+VodkaNode = Node("Black Russian",
+                 answer="Vodka",
+                 children=[BlackRussianNode1, BlackRussianNode2])
+
+option1b = Node("What base wine does master prefer?",
+                answer="Yes",
+                children=[VodkaNode, WhiskeyNode, AbsintheNode])
+option1a = Node("Dobby will be here for you.", good_result=True, answer="No")
+
+### Today's Mood
+FeelingDownNode = Node(
+  "Dobby wants to have a cocktail with master when you are feeling down.",
+  answer="No",
+  children=[option1a, option1b])
+HappyRelaxedNode = Node(" Want a cocktail, master?",
+                        answer="Yes",
+                        children=[option2a, option2b])
+
+root = Node("Does master feel happy or relaxed today?",
+            children=[FeelingDownNode, HappyRelaxedNode])
 
 
 ## 3. Create GuessOptionsView Class
@@ -38,10 +96,13 @@ class GuessOptionView(View):
 
   async def handleButtonPress(self, interaction, node):
     if (node.children == []):
-      # make a guess
-      await interaction.response.send_message(
-        content=f'Is the animal you are thinking of...{node.value}',
-        view=WrongView(node))
+      if (node.good_result == False):
+        # make a guess
+        await interaction.response.send_message(content=f'Hmm...{node.value}',
+                                                view=WrongView(node))
+      else:
+        await interaction.response.send_message(content=node.value)
+
     else:
       await interaction.response.send_message(content=node.value,
                                               view=GuessOptionView(node))
@@ -79,12 +140,11 @@ class FeedbackModal(Modal, title="Feedback"):
 
   def __init__(self, node):
     super().__init__()
-    self.newAnimal = TextInput(label="What animal were you think of?")
-    self.newQuestion = TextInput(
-      label="A question to distinguish from the guess?")
-    self.oldAnswer = TextInput(label="What answer gets to the guess?")
-    self.newAnswer = TextInput(label="What answer gets to your animal?")
-    self.add_item(self.newAnimal)
+    self.newCocktail = TextInput(label="Describe your mood today?")
+    self.newQuestion = TextInput(label="Describe your favorite flavor?")
+    self.oldAnswer = TextInput(label="Enter a base wine?")
+    self.newAnswer = TextInput(label="Enter the name of this cocktail?")
+    self.add_item(self.newCocktail)
     self.add_item(self.newQuestion)
     self.add_item(self.oldAnswer)
     self.add_item(self.newAnswer)
@@ -92,7 +152,7 @@ class FeedbackModal(Modal, title="Feedback"):
 
   async def on_submit(self, interaction):
     newNode1 = Node(self.node.value, answer=self.oldAnswer.value)
-    newNode2 = Node(self.newAnimal.value, answer=self.newAnswer.value)
+    newNode2 = Node(self.newCocktail.value, answer=self.newAnswer.value)
     self.node.value = self.newQuestion.value
     self.node.children = [newNode1, newNode2]
     await interaction.response.send_message(
@@ -117,7 +177,7 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('Hi, Dobby! How is it going?'):
+  if message.content.startswith('$EnjoyCocktail'):
     await message.channel.send(content=root.value, view=GuessOptionView(root))
     # root here is the node passed into the guessoptionview()
 
